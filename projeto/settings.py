c93580dfa1490d082068de0f074906dc035adf4b
gfmggb <90582233+gfmggb@users.noreply.github.com>
2021-09-17 01:55:06 -0300

    teste 5

--- projeto/settings.py
+++ projeto/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')4vk-ym8m2)+w$o8fs*4ml!1*+-9n7+q%x9i_@$$&9b*#gyq7^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
