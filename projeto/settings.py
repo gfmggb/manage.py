9bb8c9d82b41b096d5203b0a28564d961cffedba
gfmggb <90582233+gfmggb@users.noreply.github.com>
2021-09-18 19:35:00 -0300

    Update settings.py

--- projeto/settings.py
+++ projeto/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -135,3 +135,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
